0998e4ef8387c05c5ca21c219484529f7dcc4975
bechar <bechar.kanjariya@hitachiconsulting.com>
2018-08-29 20:03:59 +0530

    - Added link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC5nmIBFmy8r2VRI8rWQUrFNBzVnrniDmM',
+    authDomain: 'angular-6-firebase-demo.firebaseapp.com',
+    databaseURL: 'https://angular-6-firebase-demo.firebaseio.com',
+    projectId: 'angular-6-firebase-demo',
+    storageBucket: '',
+    messagingSenderId: '396550778833'
+  }
 };
